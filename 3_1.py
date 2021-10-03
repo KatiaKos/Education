52b440be45c496453c6c28c7a6eb1ffd4b2b7ff2
Ekaterina <katia_kos@mail.ru>
2021-10-03 22:26:56 +0300

    add my hw3

--- 3_1.py
+++ 3_1.py
@@ -2,11 +2,12 @@
 #1. Реализовать функцию, принимающую два числа
 # (позиционные аргументы) и выполняющую их деление.
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
-user_1 = int(input("Введите число"))
-user_2 = int(input(f"Pазделить {user_1} на :  "))
-if user_2 == 0:
-    print = int(input('разделить на нуль нельзя. Введите другое число :')
+arg_1 = int(input("Введите число  "))
+arg_2 = int(input(f" {arg_1} разделить на   "))
+
 def func_devision(arg_1, arg_2):
-   result = arg_1/arg_2
-   return result
-print(func_devision(user_1, user_2))
+    if arg_2 == 0:
+          print = int(input('разделить на нуль нельзя. Введите другое число :'))
+    else:
+        return  arg_1/arg_2
+print(func_devision(arg_1, arg_2))
